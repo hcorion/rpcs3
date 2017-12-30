a40147826aca5e46306f0a940ee7b71b4d8355e1
elad <elad3356p@gmail.com>
2017-12-30 11:08:37 +0200

    spu/hle: improve sys_dbg
    ~ add sys_dbg_get_console_type
    
    may help with games trying to load debug libraries or do nothing , cant hurt :)
    
    ~ add sys_dbg_read_spu_thread_context2
    
    for saving and restoring spu thread context

--- rpcs3/Emu/Cell/lv2/sys_dbg.h
+++ rpcs3/Emu/Cell/lv2/sys_dbg.h
@@ -1,1 +1,19 @@
 #pragma once
+
+/* Data structure for SPU thread context */
+typedef struct {
+  u128	gpr[128];		/* GPR[0-127]*/
+  u32		npc;				/* Next Program Counter */
+  u128	fpscr;				/* FPSCR */
+  u32		srr0;				/* SRR0 */
+  u32		spu_status;			/* SPU STATUS */
+  u64		spu_cfg;			/* SPU CFG */
+  u32		mb_stat;			/* MB_stat */
+  u32		ppu_mb;				/* PPU Mail Box */
+  u32		spu_mb[4];		/* SPU Mail Box */
+  u32	decrementer;			/* Decrementer */
+  //u64		mfc_cq_sr[16];	/* MFC Command Queue Save/Restore Register */
+} sys_dbg_spu_thread_context2_t;
+
+void sys_dbg_get_console_type(vm::ps3::ptr<u64> Console_Type);
+error_code sys_dbg_read_spu_thread_context2(u32 id, vm::ps3::ptr<sys_dbg_spu_thread_context2_t> spu_context);
